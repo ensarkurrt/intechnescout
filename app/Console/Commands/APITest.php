1201ff08093ebb12410408c06934e4d142a7bc3f
Ensar KURT <37beykozlu37@gmail.com>
2022-02-26 22:10:14 +0300

    Created helpers for api controller

---
+++ app/Console/Commands/APITest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\FRCApiController;
+use Illuminate\Console\Command;
+
+class APITest extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        /* FRCApiController::update_season_summary();
+        FRCApiController::update_events();
+        FRCApiController::update_teams(); */
+        /* FRCApiController::test(); */
+        FRCApiController::update_images();
+    }
+}
---
+++ app/Enums/ApiPath.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+class ApiPath
+{
+    const Events =   'events';
+    const SeasonSummary =   '';
+    const TeamListing =   'teams';
+    const TeamMedia =   'avatars';
+}
