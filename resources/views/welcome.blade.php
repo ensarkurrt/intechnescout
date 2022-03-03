a84ebb9e97e839845c27e8081b8e6a0ac94f3c4f
Ensar KURT <37beykozlu37@gmail.com>
2022-03-03 11:38:26 +0300

    design changes on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
                     <img src="{{asset('storage/media/logo.png')}}" style="width: 20rem;max-width:20rem;min-width:10rem"/>
                 </div>
 
-                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow rounded-xl mx-6">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
                             <div class="flex items-center">
